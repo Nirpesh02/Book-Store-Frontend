2f08890303a6dc84683f990c80e93683bd6bcfed
Nirpesh02 <nirpeshdhungel7@gmail.com>
2026-08-16 13:28:35 +0545

    change

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 // API helper for connecting frontend to backend
-const API_BASE = 'https://book-store-backend-39qh.onrender.com/api';
+const API_BASE = 'http://localhost:5000/api';
 
 // Get stored token
 const getToken = () => sessionStorage.getItem('bookverse_token');
